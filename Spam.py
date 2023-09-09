d54d5e21dc249daad3bbc3f8bf8a8f6e942574c3
Arman-Bisht <112895246+Arman-Bisht@users.noreply.github.com>
2023-09-09 20:52:00 +0530

    whatsapp_spammer

---
+++ Spam.py
@@ -0,0 +1,9 @@
+import pyautogui
+import webbrowser as wb
+import time
+wb.open ( " web.whatsapp.com " )
+
+for i in range ( 10 ) :
+    pyautogui.press ( " h " )
+    pyautogui.press ( " i " )
+    pyautogui.press ( " enter " )
